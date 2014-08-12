714b850795a6c50a5fa0ccb882c9de69d3494144
David Jones <drj@pobox.com>
2014-08-12 14:29:28 +0100

    Use wpcf7_posted_data filter

--- wpdatarequest.php
+++ wpdatarequest.php
@@ -9,16 +9,20 @@
 * Use the only wpcf7 hook we could find, setting priority to 1
 * so it executes before any other (eg: database-saving) plugins.
 */
-add_action('wpcf7_before_send_mail', 'my_conversion', 1);
+add_filter('wpcf7_posted_data', 'sw_add_referencenumber');
+/* add_action('wpcf7_before_send_mail', 'sw_add_referencenumber', 1);
+*/
 
-function my_conversion(&$cf7){
+function sw_add_referencenumber($posted_data){
     global $wpdb;
 
+    $posted_data["drjtest"] = "hello";
     $tableName = $wpdb->prefix . 'cf7dbplugin_submits';
-	$query = "SELECT MAX(`field_value`) FROM `$tableName` WHERE `field_name`='referencenumber'";
-	$result = $wpdb->get_var($query);
+    $query = "SELECT MAX(`field_value`) FROM `$tableName` WHERE `field_name`='referencenumber'";
+    $result = $wpdb->get_var($query);
 
-	/* Insert snarky comment about race conditions here */
-    $cf7->posted_data["referencenumber"] = $result + 1;
+    /* Insert snarky comment about race conditions here */
+    $posted_data["referencenumber"] = $result + 1;
 
+    return $posted_data;
 }
